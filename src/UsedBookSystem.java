552556d843e7ba19b52994ea647e2cefcf7c1991
zi <frankmk830@gmail.com>
2019-03-17 12:39:26 -0400

    prompt will be different when user bus or sells the first book

--- src/UsedBookSystem.java
+++ src/UsedBookSystem.java
@@ -11,25 +11,31 @@ public class UsedBookSystem{
         // initialize Books list
         Inventory inventory = new Inventory();
         boolean quitFlag = false;
+        boolean firstAttempt = true;
 
         /*
          * Need to make a loop here, so the program will keep asking user if they want
          * to buy until they say they want to quit the program.
          */
+        
         while(quitFlag == false){
-            String modeSelected = mode();
+
+            String modeSelected = mode(firstAttempt);
     
             //do all the buying and selling  
             switch(modeSelected){
                 case "b":
+                    firstAttempt = false;
                     Buy newBuy = new Buy(inventory);
                     newBuy.printBuy();
                     break;
                 case "s":
+                    firstAttempt = false;
                     Sell newSell = new Sell();
                     newSell.sell(inventory);
                     break;
                 case "q":
+                    firstAttempt = false;
                     quitFlag = true;
                     break;
                 default:
@@ -42,13 +48,24 @@ public class UsedBookSystem{
         inventory.printInventory();
     }
 
-    public static String mode(){
+    public static String mode(boolean firstAttempt){
         //ask user if they want to sell or buy
         String userInput = "";
         do{
-            System.out.println("******************");
-            System.out.println("Thank you for choosing us! \nWould you like to buy or sell your book? \nPlease enter \"buy\" or \"b\" for buy, \"sell\" or \"s\" for sell, or \"quit\" or \"q\" to exit the program");
-            System.out.println("******************");
+            if(firstAttempt){
+                //Welcome the user when they enter the program first time
+                System.out.println("******************");
+                System.out.println("Welcome to the used book exchanged system!!!");
+                System.out.println(
+                        "Would you like to buy or sell your book? \nPlease enter \"buy\" or \"b\" for buy, \"sell\" or \"s\" for sell, or \"quit\" or \"q\" to exit the program");
+                System.out.println("******************");
+            }else{
+                System.out.println("******************");
+                System.out.println(
+                        "Would you like to buy or sell another book? \nPlease enter \"buy\" or \"b\" for buy, \"sell\" or \"s\" for sell, or \"quit\" or \"q\" to exit the program");
+                System.out.println("******************");
+            }
+
             Scanner input = new Scanner(System.in);
             userInput = input.nextLine();
             //make all string in lowercase for switch statement in main method
