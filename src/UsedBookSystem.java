1f8fb609f10519ce6569a1ad880ef1ffe3f83c13
zi <frankmk830@gmail.com>
2019-03-17 01:05:27 -0400

    we can buy and sell until we tell the program to stop now

--- src/UsedBookSystem.java
+++ src/UsedBookSystem.java
@@ -8,27 +8,34 @@ import java.util.Scanner;
 public class UsedBookSystem{
     public static void main(String[] args){
 
-        //ask whether user wants to buy or sell
-        String modeSelected = mode();
-       
-        //initialize Books list
+        // initialize Books list
         Inventory inventory = new Inventory();
+        boolean quitFlag = false;
 
-        //do all the buying and selling
-        switch(modeSelected){
-            case "buy":
-            case "b":
-                Buy newBuy = new Buy(inventory);
-                newBuy.printBuy();
-                break;
-            case "sell":
-            case "s":
-                Sell newSell = new Sell();
-                newSell.sell(inventory);
-                break;
-            default:
-                System.out.println("Please select buy or sell");
-                break;
+        /*
+         * Need to make a loop here, so the program will keep asking user if they want
+         * to buy until they say they want to quit the program.
+         */
+        while(quitFlag == false){
+            String modeSelected = mode();
+    
+            //do all the buying and selling  
+            switch(modeSelected){
+                case "b":
+                    Buy newBuy = new Buy(inventory);
+                    newBuy.printBuy();
+                    break;
+                case "s":
+                    Sell newSell = new Sell();
+                    newSell.sell(inventory);
+                    break;
+                case "q":
+                    quitFlag = true;
+                    break;
+                default:
+                    System.out.println("Please select buy or sell");
+                    break;
+            }
         }
 
         //add, edit, or delete book in the inventory.
@@ -37,13 +44,35 @@ public class UsedBookSystem{
 
     public static String mode(){
         //ask user if they want to sell or buy
-        System.out.println("Thank you for choosing us! Would you like to buy or sell your book? Enter \"buy\" or \"b\" for buy, or \"sell\" or \"s\" for sell.");
+        String userInput = "";
+        do{
+        System.out.println("******************");
+        System.out.println("Thank you for choosing us! \nWould you like to buy or sell your book? \nPlease enter \"buy\" or \"b\" for buy, \"sell\" or \"s\" for sell, or \"quit\" or \"q\" to exit the program");
+        System.out.println("******************");
         Scanner input = new Scanner(System.in);
-        String userInput = input.nextLine();
+        userInput = input.nextLine();
         //make all string in lowercase for switch statement in main method
-        //close scanner
-        //input.close();
         userInput = userInput.toLowerCase();
+
+        //turn the user input into 1 letter, easy to process for later.
+        switch (userInput) {
+            case "b":
+            case "buy":
+                userInput = "b";
+                break;
+            case "s":
+            case "sell":
+                userInput = "s";
+                break;
+            case "q":
+            case "quit":
+                userInput = "q";
+                break;
+            default:
+                break;
+        }
+        }while(userInput!="b" && userInput != "s" && userInput != "q");
+
         return userInput;
     }
     
